073caaeb9e5e3a66ce3ac482e418ddf7ec6cc526
DonSteve <aabad01@ucm.es>
2020-06-08 19:59:59 +0200

    Update createuser.sql

--- SWConsiguelow/mysql/createuser.sql
+++ SWConsiguelow/mysql/createuser.sql
@@ -1,2 +1,3 @@
 CREATE USER 'consiguelow'@'localhost' IDENTIFIED BY 'consiguelow';
 GRANT ALL PRIVILEGES ON `consiguelow`.* TO 'consiguelow'@'localhost';
+/*grant all on db_example.* to 'springuser'@'%';*/
