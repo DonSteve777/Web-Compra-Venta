5f32388bc96badbe15bcbba6a40307356e502ca8
DonSteve <aabad01@ucm.es>
2020-06-19 18:38:25 +0200

    vistas no usadas

--- SWConsiguelow/logout.php
+++ SWConsiguelow/logout.php
@@ -3,28 +3,9 @@ require_once __DIR__.'/includes/config.php';
 use es\fdi\ucm\aw\Aplicacion as App;
 
 App::getSingleton()->logout();
+
+    echo '<script type="text/javascript">
+    alert("Se ha cerrado la sesion");
+    window.location.assign("index.php");
+    </script>';
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="styles/style.css" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Cerrar sesion</title>
-    </head>
-    <body>
-        <div id="contenedor">
-            <?php
-                require("includes/common/cabecera.php");
-            ?>
-            <div id="contenido">
-                <h1>Cerrar sesion</h1>
-            <?php
-                echo '<script type="text/javascript">
-                alert("Se ha cerrado la sesion");
-                window.location.assign("index.php");
-                </script>';
-             ?>
-            </div>
-        </div>
-	</body>
-</html>
