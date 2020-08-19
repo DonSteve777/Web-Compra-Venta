12f751d22e960331572745275e61c64e9d685cf8
DonSteve <aabad01@ucm.es>
2020-08-19 21:31:24 +0200

    más responsive y scripts de js añadidos

--- SWConsiguelow/vender.php
+++ SWConsiguelow/vender.php
@@ -28,26 +28,32 @@ EOF;
     <title>Local Consiguelow</title>
     <link rel="icon" href="img/money.ico"/>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+    <script src="js/productoDetalle.js"></script> 
 </head>
     <body>
         <?php
             require("includes/common/cabecera.php");
         ?>
-        <div class="row">
-            <div class="col-4">
-            </div>
-            <div class="col-4">
-                <div class="d-flex flex-column bg-light m-3">
-                    <h1 class="m-2">Subir un producto</h1>
-                    <?php
-                        echo vender();
-                        
-                    ?>
-                    <?php require("includes/common/footer.php"); ?>
+        <div class="container-fluid bg-light">
+            <div class="row">
+                <div class="col-md-4 col-12">
                 </div>
+                <div class="col-md-4 col-12">
+                    <div class="d-flex flex-column bg-light mt-3">
+                        <h1 class="m-2">Subir un producto</h1>
+                        <?php
+                            echo vender();      
+                        ?>
+                        <?php require("includes/common/footer.php"); ?>
+                    </div>
+                </div>
+                <div class="col-md-4 col-12">
             </div>
-            <div class="col-4">
-            
         </div>
         
     </body>
